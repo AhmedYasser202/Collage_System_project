89f58c42e8ddbb48acd5f3850ef0a106768d7d0c
Ahmed_Yasser <ahmedyassercomp@gmail.com>
2024-01-01 22:13:10 +0200

    Show student teacher course By ID (Controller)

--- College_System/Collage_System/Controller.cpp
+++ College_System/Collage_System/Controller.cpp
@@ -4,11 +4,24 @@
 class StudentController
 {
 private:
-	StudentServiceImpl studentSrevice;
+	StudentServiceImpl studentService;
+	student studentResult;
 public:
 	int addStudent(student std)
 	{
-		return studentSrevice.addStudent(std);
+		return studentService.addStudent(std);
+	}
+	void showStudentById(int id)
+	{
+		studentResult = studentService.getStudentByID(id);
+		
+		if (studentResult.getId() != -1)
+		{
+			cout << "\t\t ID \t\t Name \t\t Age \t\t Phone Number \t\t GPA\n";
+			cout << "\t\t" << studentResult.getId() << "\t\t" << studentResult.getName()
+				<< "\t\t" << studentResult.getAge() << "\t\t" << studentResult.getPhoneNumber()
+				<< "\t\t" << studentResult.getGpa() << "\n";
+		}
 	}
 };
 
@@ -17,11 +30,25 @@ public:
 class CourseController
 {
 private:
-	CourseServiceImpl courseSrevice;
+	CourseServiceImpl courseService;
+	Course courseResult;
+	
 public:
 	int addCourse(Course course)
 	{
-		return courseSrevice.addCourse(course);
+		return courseService.addCourse(course);
+	}
+	
+	void showCourseById(int id)
+	{
+		courseResult = courseService.getCourseByID(id);
+
+		if (courseResult.getId() != -1)
+		{
+			cout << "\t\t ID \t\t Name \t\t Hours\n";
+			cout << "\t\t" << courseResult.getId() << "\t\t" << courseResult.getName()
+				<< "\t\t" <<courseResult.getHour() <<"\n";
+		}
 	}
 };
 
@@ -31,11 +58,25 @@ class TeacherController
 {
 private:
 	TeacherServiceImpl teacherService;
+	Teacher teacherResult;
 
 public:
 	int addTeacher(Teacher teacher)
 	{
 		return teacherService.addTeacher(teacher);
+	}
+
+	void showTeacherById(int id)
+	{
+		teacherResult = teacherService.getTeacherByID(id);
+
+		if (teacherResult.getId() != -1)
+		{
+			cout << "\t\t ID \t\t Name \t\t Age \t\t Phone Number \t\t Salary\n";
+			cout << "\t\t" << teacherResult.getId() << "\t\t" << teacherResult.getName()
+				<< "\t\t" << teacherResult.getAge() << "\t\t" << teacherResult.getPhoneNumber()
+				<< "\t\t" << teacherResult.getSalary() << "\n";
+		}
 	}
 };
 
